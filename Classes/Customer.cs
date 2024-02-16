d71affb6acf052a452a910fbe1a70a6327e3c9d9
İsmet SAYIN <98154124+ismetsayin@users.noreply.github.com>
2024-02-16 20:13:44 +0300

    Add files via upload

---
+++ Classes/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Customer // Müşterinin özellikleri / bilgileri
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer = new Customer();
+            customer.City = "Ankara";
+            customer.ID = 1;
+            customer.FirstName = "İsmet";
+            customer.LastName = "Sayın";
+
+            Customer customer2 = new Customer
+            {
+                ID = 2, City="İstanbul", FirstName="Ali", LastName="Korkal"
+            };
+
+            Console.WriteLine(customer2.FirstName);
+
+            Console.ReadLine();
+        }
+    }
+}
